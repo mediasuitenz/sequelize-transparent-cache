244d093f02dbc644ec0df0910ca498eb68298548
Steve Foster <steve.foster@madecurious.com>
2026-05-28 13:28:58 +1200

    feat: changes to support sequelize v6

--- packages/sequelize-transparent-cache/src/methods/class.js
+++ packages/sequelize-transparent-cache/src/methods/class.js
@@ -1,72 +1,69 @@
 const cache = require('../cache')
 
-function buildAutoMethods (client, model) {
+function buildAutoMethods(client, model) {
   return {
-    client () {
+    client() {
       return client
     },
-    create () {
-      return model.create.apply(model, arguments)
-        .then(instance => {
-          return cache.save(client, instance)
-        })
-    },
-    findByPk (id) {
-      return cache.get(client, model, id)
-        .then(instance => {
-          if (instance) {
-            return instance
-          }
-
-          return (model.findByPk || model.findById).apply(model, arguments)
-            .then(instance => cache.save(client, instance))
-        })
-    },
-    findById () {
-      return this.findByPk.apply(this, arguments)
-    },
-    upsert (data) {
-      return model.upsert.apply(model, arguments).then(created => {
-        return cache.destroy(client, model.build(data))
-          .then(() => created)
+    create() {
+      return model.create.apply(model, arguments).then((instance) => {
+        return cache.save(client, instance)
       })
     },
-    insertOrUpdate () {
+    findByPk(id) {
+      return cache.get(client, model, id).then((instance) => {
+        if (instance) {
+          return instance
+        }
+
+        return model.findByPk.apply(model, arguments).then((instance) => cache.save(client, instance))
+      })
+    },
+    findById() {
+      return this.findByPk.apply(this, arguments)
+    },
+    upsert(data) {
+      return model.upsert.apply(model, arguments).then((result) => {
+        // v6 returns [instance, created]; v5 returns a boolean
+        const instance = Array.isArray(result) ? result[0] : model.build(data)
+        return cache.destroy(client, instance).then(() => result)
+      })
+    },
+    insertOrUpdate() {
       return this.upsert.apply(this, arguments)
-    }
+    },
   }
 }
 
-function buildManualMethods (client, model, customKey) {
+function buildManualMethods(client, model, customKey) {
   return {
-    client () {
+    client() {
       return client
     },
-    findAll () {
-      return cache.getAll(client, model, customKey)
-        .then(instances => {
-          if (instances) { // any array - cache hit
-            return instances
-          }
+    findAll() {
+      return cache.getAll(client, model, customKey).then((instances) => {
+        if (instances) {
+          // any array - cache hit
+          return instances
+        }
 
-          return model.findAll.apply(model, arguments)
-            .then(instances => cache.saveAll(client, model, instances, customKey))
-        })
+        return model.findAll
+          .apply(model, arguments)
+          .then((instances) => cache.saveAll(client, model, instances, customKey))
+      })
     },
-    findOne () {
-      return cache.get(client, model, customKey)
-        .then(instance => {
-          if (instance) {
-            return instance
-          }
+    findOne() {
+      return cache.get(client, model, customKey).then((instance) => {
+        if (instance) {
+          return instance
+        }
 
-          return model.findOne.apply(model, arguments)
-            .then(instance => cache.save(client, instance, customKey))
-        })
+        return model.findOne.apply(model, arguments).then((instance) => cache.save(client, instance, customKey))
+      })
     },
-    clear () {
+    clear() {
       return cache.clearKey(client, model, customKey)
-    }
+    },
   }
 }
 
